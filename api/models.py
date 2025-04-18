6ade7ea07a3b3517b3da2e37a34b9a429216d9b7
FARiDHAMiD <faridmohamedfarid@gmail.com>
2025-04-18 18:02:01 +0200

    profile signal update

--- api/models.py
+++ api/models.py
@@ -32,13 +32,13 @@ class Department(models.Model):
 class DoctorProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="doctor_profile")
     image = models.ImageField(upload_to="doctor_images/", blank=True, null=True)
-    location = models.CharField(max_length=255)
-    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, related_name="doctors")
+    location = models.CharField(max_length=255, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, related_name="doctors", blank=True)
     title = models.CharField(max_length=255, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
-            return f'{self.user.first_name} {self.user.last_name} - {self.department.name}'
+            return f'{self.user.first_name} {self.user.last_name}'
 
     def save(self, *args, **kwargs):
         # Check if the object already exists in the database
--- api/signals.py
+++ api/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from django.db import models
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from .models import DoctorProfile, User, PatientProfile
 
 from django.core.files.storage import default_storage
@@ -18,3 +18,27 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
 def create_patient_profile(sender, instance, created, **kwargs):
     if created:  # Ensure it only runs on creation
         PatientProfile.objects.create(user=instance)
+
+
+# change role from patient to doctor / admin will delete patient profile
+@receiver(pre_save, sender=User)
+def update_user_profile(sender, instance, **kwargs):
+    if not instance.pk:
+        return  # New user handled in post_save
+
+    old_user = User.objects.get(pk=instance.pk)
+    old_role = old_user.role
+    new_role = instance.role
+
+    if old_role != new_role:
+        # Delete old profile
+        if old_role == User.Role.PATIENT:
+            PatientProfile.objects.filter(user=instance).delete()
+        elif old_role == User.Role.DOCTOR:
+            DoctorProfile.objects.filter(user=instance).delete()
+
+        # Create new profile
+        if new_role == User.Role.PATIENT:
+            PatientProfile.objects.get_or_create(user=instance)
+        elif new_role == User.Role.DOCTOR:
+            DoctorProfile.objects.get_or_create(user=instance)
